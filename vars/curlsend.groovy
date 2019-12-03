575cb9407a796f2b14167627f5503cf0c0323480
Staudenmaier <baustelleninfo@rocketmail.com>
2019-12-03 17:01:39 +0100

    remove '

--- vars/curlsend.groovy
+++ vars/curlsend.groovy
@@ -4,7 +4,7 @@ def call(String name = 'klaus') {
   echo "-----------------------------"
   echo "${env.JOB_NAME}"
   echo "-----------------------------"
-  sh 'curl -XPUT "http://localhost:9200/elapstimemeter_master/_doc/gitcommit_sha" -H 'Content-Type: application/json' -d'
+  sh 'curl -XPUT http://localhost:9200/elapstimemeter_master/_doc/gitcommit_sha -H Content-Type: application/json -d
 	{
 	"Buildstatus": "success",
 	"BuildDuration": 12345,
@@ -18,5 +18,5 @@ def call(String name = 'klaus') {
 			"spawntime": 6
 		}
 		]
-	}''
+	}'
 }
