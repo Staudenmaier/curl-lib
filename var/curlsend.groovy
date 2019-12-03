e5461a72c8a9abf632e960e34a8d9c3bd91adfb0
Staudenmaier <baustelleninfo@rocketmail.com>
2019-12-03 10:12:53 +0100

    Update curlsend.groovy

--- var/curlsend.groovy
+++ var/curlsend.groovy
@@ -4,17 +4,6 @@ def call(String name = 'human') {
   echo "-----------------------------"
   echo "${env.JOB_NAME}"
   echo "-----------------------------"
-  sh 'curl -H "Content-Type: application/json" -XPOST "http://10.243.180.253:12003/tes123/doc" -d "{ \"field\" : \"value\"}"'
+  sh 'curl -H "Content-Type: application/json" -XPOST "http://10.243.180.253:12003/_doc/tes123" -d "{ \"field\" : \"value\"}"'
   echo "-----------------------------"
-
-  
-  def url = new URL('http://google.de')
-  def connection = url.openConnection()
-  connection.requestMethod = 'GET'
-  if (connection.responseCode == 200) {
-    println connection.content.text
-    println connection.contentType
-    println connection.lastModified
-    connection.headerFields.each { println "> ${it}"}
-  }
 }
