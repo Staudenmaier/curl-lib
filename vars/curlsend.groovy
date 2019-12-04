e2b2c612a27717b6a3edec5b3090676b29e8fdc7
Staudenmaier <baustelleninfo@rocketmail.com>
2019-12-04 15:32:41 +0100

    Update curlsend.groovy

--- vars/curlsend.groovy
+++ vars/curlsend.groovy
@@ -5,36 +5,7 @@ import static groovyx.net.http.ContentType.JSON
 import static groovyx.net.http.Method.POST
 import groovyx.net.http.HTTPBuilder
 
+import com.elektrobit.JenkinsHttpClient
 
 def call(String name = 'klaus') {
-  JenkinsHttpClient httpxxx;
-  echo "Hello, ${name}."
-  echo "-----------------------------"
-  echo "${env.JOB_NAME}"
-  echo "-----------------------------"
-  
-  String targetUrl = 'http://10.243.180.253'
-  def http = new HTTPBuilder(targetUrl)
-  http.request(POST) {
-  	uri.path = "/elapstimemeter_master/_doc/gitcommit_sha"
-  	requestContentType = JSON
-  	body = [  
-        	"Buildstatus": "success",
-        	"BuildDuration": 12345,
-        	"artifact_size": 90000,
-  	]
-  	//headers.'Authorization' = ""
-  	headers.'User-Agent' = 'Logstash'
-  	headers.Accept = 'application/json'
-/*
-  	response.success = { resp, json ->
-    		println "GitHub updated successfully! ${resp.status}"
-  	}
-
-  	response.failure = { resp, json ->
-    		println "GitHub update Failure! ${resp.status} " + json.message
-  	}
-*/
-  }
-	
 }
