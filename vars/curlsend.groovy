6d276a7e56216af021a4531d88f0fca6ca162001
Staudenmaier <baustelleninfo@rocketmail.com>
2019-12-04 15:02:59 +0100

    Update curlsend.groovy

--- vars/curlsend.groovy
+++ vars/curlsend.groovy
@@ -1,4 +1,6 @@
 #!/usr/bin/env groovy
+@Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.7')
+
 import groovyx.net.http.ContentType
 
 def call(String name = 'klaus') {
