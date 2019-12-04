3deccbc001ed99f96e43438694a77f04d48e07a2
Staudenmaier <baustelleninfo@rocketmail.com>
2019-12-04 16:35:33 +0100

    Update curlsend.groovy

--- vars/curlsend.groovy
+++ vars/curlsend.groovy
@@ -10,19 +10,23 @@ import groovy.json.JsonOutput
 import com.elektrobit.JenkinsHttpClient
 
 def call(String name = 'klaus') {
-  echo "Hello, ${name}."
+  echo "http://10.243.180.253:12003/${env.JOB_NAME}/_doc/${env.GIT_COMMIT}"
   echo "-----------------------------"
   echo "${env.JOB_NAME}"
   echo "${env.GIT_COMMIT}"
-  echo "${env.GIT_COMMIT}"
   echo "----------------------------- "
-  echo printenv
   
   def jsonSlurper = new JsonSlurper()
-  def object = jsonSlurper.parseText('{ "name": "John Doe" }')
+  def object = jsonSlurper.parseText('{ 
+                                        "BUILD_NUMBER": env.BUILD_NUMBER
+                                        "Buildstatus": "success",
+                                        "BuildDuration": 12345,
+                                        "artifact_size": 90000,
+                                      }
+                                     ')
                                    
 
   JenkinsHttpClient client = new JenkinsHttpClient()
-  client.postJson("http://10.243.180.253:12003/${env.JOB_NAME}/_doc/", JsonOutput.toJson(object)) 
+  client.postJson("http://10.243.180.253:12003/${env.JOB_NAME}/_doc/${env.GIT_COMMIT}", JsonOutput.toJson(object)) 
   
 }
