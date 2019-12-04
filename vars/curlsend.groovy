6ca91cd6afba8eb21958520f1773eeca2fb57460
Staudenmaier <baustelleninfo@rocketmail.com>
2019-12-04 15:09:09 +0100

    Update curlsend.groovy

--- vars/curlsend.groovy
+++ vars/curlsend.groovy
@@ -1,23 +1,45 @@
 #!/usr/bin/env groovy
 @Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.7')
 
-import groovyx.net.http.ContentType
+import static groovyx.net.http.ContentType.JSON
+import static groovyx.net.http.Method.POST
+import groovyx.net.http.HTTPBuilder
 
 def call(String name = 'klaus') {
   echo "Hello, ${name}."
   echo "-----------------------------"
   echo "${env.JOB_NAME}"
   echo "-----------------------------"
-  uri.path = 'http://example.com/handler.php'
-    body = [name: 'bob', title: 'construction worker']
-    requestContentType = ContentType.JSON
+  
+  String targetUrl = 'http://10.243.180.253'
+  def http = new HTTPBuilder(targetUrl)
+  http.request(POST) {
+  uri.path = "/elapstimemeter_master/_doc/gitcommit_sha"
+  requestContentType = JSON
+  body = [  
+        "Buildstatus": "success",
+        "BuildDuration": 12345,
+        "artifact_size": 90000,
+        "SpawnDocker": [{
+                        "Containtername": "Container A",
+                        "spawntime": 60
+                },
+                {
+                        "Containtername": "Container B",
+                        "spawntime": 6
+                }
+        ]
+  ]
+  //headers.'Authorization' = ""
+  headers.'User-Agent' = 'Logstash'
+  headers.Accept = 'application/json'
 
-    response.success = { resp ->
-        println "Success! ${resp.status}"
-    }
+  response.success = { resp, json ->
+    println "GitHub updated successfully! ${resp.status}"
+  }
 
-    response.failure = { resp ->
-        println "Request failed with status ${resp.status}"
-    }
+  response.failure = { resp, json ->
+    println "GitHub update Failure! ${resp.status} " + json.message
+  }
 	
 }
