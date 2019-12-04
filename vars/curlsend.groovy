c8a2ac452a2a8c18ca0dd92be4eef0ed017bec3d
Staudenmaier <baustelleninfo@rocketmail.com>
2019-12-04 15:40:24 +0100

    Update curlsend.groovy

--- vars/curlsend.groovy
+++ vars/curlsend.groovy
@@ -8,4 +8,13 @@ import groovyx.net.http.HTTPBuilder
 import com.elektrobit.JenkinsHttpClient
 
 def call(String name = 'klaus') {
+  
+  def json = [
+    "Buildstatus": "success",
+    "BuildDuration": 12345,
+    "artifact_size": 90000
+  ]
+
+  JenkinsHttpClient client
+  client.postJson("http://10.243.180.253:12003/elapstimemeter_master/_doc/gitcommit_sha", json) {
 }
