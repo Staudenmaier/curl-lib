ce9d097a2a4bf358ea9f14eb80ad294fa1c5d11e
Staudenmaier <baustelleninfo@rocketmail.com>
2019-12-04 14:56:06 +0100

    Update curlsend.groovy

--- vars/curlsend.groovy
+++ vars/curlsend.groovy
@@ -4,19 +4,19 @@ def call(String name = 'klaus') {
   echo "-----------------------------"
   echo "${env.JOB_NAME}"
   echo "-----------------------------"
-  sh 'curl -XPUT "http://localhost:9200/elapstimemeter_master/_doc/gitcommit_sha" -H "Content-Type: application/json" -d"
+  sh 'curl -XPUT "http://localhost:9200/elapstimemeter_master/_doc/gitcommit_sha" -H "Content-Type: application/json" -d\'
 	{
-	\"Buildstatus\"  : \"success\",
-	\"BuildDuration\": 12345,
-	\"artifact_size\": 90000,
-	\"SpawnDocker\": [{
-			\"Containtername\": \"Container A\",
-			\"spawntime\": 60
+	"Buildstatus"  : "success",
+	"BuildDuration": 12345,
+	"artifact_size": 90000,
+	"SpawnDocker": [{
+			"Containtername\": "Container A",
+			"spawntime": 60
 		},
 		{
-			\"Containtername\": \"Container B\",
-			\"spawntime\": 6
+			"Containtername": "Container B",
+			"spawntime": 6
 		}
 		]
-	}"'
+	}\''
 }
