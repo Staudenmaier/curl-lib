bb1a863c2f9d2bcd3db0672b69704b6abae6b76d
Staudenmaier, Klaus <Klaus.Staudenmaier@elektrobit.com>
2019-12-10 00:09:32 +0100

    WIP

--- vars/curlsend.groovy
+++ vars/curlsend.groovy
@@ -32,7 +32,7 @@ def call(String name = '/generatedFile.txt') {
   ]
 
   def data = [
-    "TimeStamp" :  "${currentBuild.startTimeInMillis}",
+    "@timestamp" :  "${currentBuild.startTimeInMillis}",
     "Buildstatus": "success",
     "GitCommit SHA": "${env.GIT_COMMIT}",
     "BuildDuration": 12345,
